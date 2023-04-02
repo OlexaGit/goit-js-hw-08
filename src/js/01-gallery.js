9bcef89b9b749f19e4cd77f7de6db82f33d746a4
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-03 01:22:56 +0300

    HW-8 update 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,49 +4,26 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log(galleryItems);
 const galleryContainer = document.querySelector('ul.gallery');
-
 const addHtmlElemenys = galleryItems
   .map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"/>
-  </a>
-</li>`;
+    return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
   })
   .join('');
-
 galleryContainer.insertAdjacentHTML('beforeend', addHtmlElemenys);
-
 galleryContainer.addEventListener('click', event => {
   event.preventDefault();
 });
 
-galleryContainer.onclick = event => {
-  if (!event.target.classList.contains('gallery__image')) {
-    console.log('ERROR');
-    return;
-  }
-  const instance = basicLightbox.create(
-    `
-		<img width="1400" height="900" src="${event.target.dataset.source}">
-	`
-  );
-  instance.show();
-  console.log(event.target.dataset.source);
-  const keyEscape = evt => {
-    console.log('Keydown: ', evt.key);
-    if (evt.key === 'Escape') {
-      instance.close();
-      document.removeEventListener('keydown', keyEscape);
-    }
-  };
-
-  document.addEventListener('keydown', keyEscape);
-};
-
-console.log(galleryItems);
+/* SimpleLightbox */
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
