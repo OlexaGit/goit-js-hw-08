a9fdc8d4a8c13f2736c057458eff03876b6eacd6
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-03 18:50:31 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,52 +1,18 @@
 import Player from '@vimeo/player';
+// var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-// ====
-// {/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */}
-// {/* <script src="https://player.vimeo.com/api/player.js"></script> */}
-// {/* <script>
-//     const iframe = document.querySelector('iframe');
-//     const player = new Vimeo.Player(iframe);
-
-//     player.on('play', function() {
-//         console.log('played the video!');
-//     });
-
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
-// </script> */}
-const onPlay = function (data) {
+const onPlay = function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
   // data is an object containing properties specific to that event
 };
 
-player.on('play', onPlay);
-// ===
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+player.on('play', throttle(onPlay, 1000));
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const time = localStorage.getItem('videoplayer-current-time');
+console.log(time);
+
+player.setCurrentTime(time || 0);
