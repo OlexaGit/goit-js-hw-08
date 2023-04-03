1534fb6019b6b006ed3aaeb925f1978af02f0046
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-03 13:59:50 +0300

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -15,7 +15,6 @@ player.getVideoTitle().then(function (title) {
 });
 // ====
 // {/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */}
-
 // {/* <script src="https://player.vimeo.com/api/player.js"></script> */}
 // {/* <script>
 //     const iframe = document.querySelector('iframe');
