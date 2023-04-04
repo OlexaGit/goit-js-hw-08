bfd084211e94ce3b4f00d37f3e1d141b52e44957
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-04 11:15:17 +0300

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', event => {
+  event.preventDefault();
+  const {
+    elements: { username, password },
+  } = event.currentTarget;
+  console.log(username.value, password.value);
+});
