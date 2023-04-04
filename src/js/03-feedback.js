20f27fcb3f2a255391e631147da30e331df2ef97
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-04 11:59:32 +0300

    up-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-// const form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 console.log('hello');
 // form.addEventListener('input', event => {
 //   event.preventDefault();
@@ -8,24 +8,24 @@ console.log('hello');
 //   console.log(username.value, password.value);
 // });
 
-// form.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
-// function onFormSubmit(event) {
-//     event.preventDefault();
-//   console.log('hello');
-//   const formElements = event.currentTarget.elements;
-//   console.log(formElements);
-//   const email = formElements.email.value;
-//   console.log(email);
-//   const message = formElements.message.value;
-//   if (email.length === 0 || message.length === 0) {
-//     alert('Please fill in all fields');
-//   } else {
-//     const formData = {
-//       email,
-//       message,
-//     };
-//     console.log(formData);
-//     form.reset();
-//   }
-// }
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log('hello');
+  const formElements = event.currentTarget.elements;
+  console.log(formElements);
+  const email = formElements.email.value;
+  console.log(email);
+  const message = formElements.message.value;
+  if (email.length === 0 || message.length === 0) {
+    alert('Please fill in all fields');
+  } else {
+    const formData = {
+      email,
+      message,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
