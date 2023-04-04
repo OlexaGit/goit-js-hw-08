09ef7b3604f37b3b047ce5b8bd684d49870685ec
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-04 08:15:47 +0000

    Deploying to gh-pages from @ OlexaGit/goit-js-hw-08@bfd084211e94ce3b4f00d37f3e1d141b52e44957 🚀

---
+++ 03-feedback.239d150a.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){e.preventDefault();var n=e.currentTarget.elements,r=n.username,t=n.password;console.log(r.value,t.value)}));
+//# sourceMappingURL=03-feedback.239d150a.js.map
---
+++ 03-feedback.5a969950.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(e=>{e.preventDefault();const{elements:{username:t,password:n}}=e.currentTarget;console.log(t.value,n.value)}));
+//# sourceMappingURL=03-feedback.5a969950.js.map
