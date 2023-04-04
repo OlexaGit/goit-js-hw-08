1b7f77727f4ed9199126ddf38f3d1bdc41b3e4cf
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-04 16:35:57 +0300

    update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,96 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
-console.log('hello');
-// form.addEventListener('input', event => {
-//   event.preventDefault();
-//   const {
-//     elements: { username, password },
-//   } = event.currentTarget;
-//   console.log(username.value, password.value);
-// });
+const inputElement = document.querySelector('input');
+const messageElement = document.querySelector('textarea');
 
-form.addEventListener('submit', onFormSubmit);
+inputElement.addEventListener('input', throttle(addStorageItemEmail, 500));
+messageElement.addEventListener('input', throttle(addStorageItemEmail, 500));
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  console.log('hello');
-  const formElements = event.currentTarget.elements;
-  console.log(formElements);
-  const email = formElements.email.value;
-  console.log(email);
-  const message = formElements.message.value;
-  if (email.length === 0 || message.length === 0) {
-    alert('Please fill in all fields');
-  } else {
-    const formData = {
-      email,
-      message,
-    };
-    console.log(formData);
-    form.reset();
+function addStorageItemEmail() {
+  const emailValue = inputElement.value;
+  console.dir(emailValue);
+  const messageValue = messageElement.value;
+  console.dir(messageValue);
+  const formData = {
+    email: emailValue,
+    message: messageValue,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function readStorageItemEmail() {
+  const saveFormData = localStorage.getItem('feedback-form-state');
+  if (saveFormData) {
+    const formData = JSON.parse(saveFormData);
+    inputElement.value = formData.email;
+    messageElement.value = formData.message;
   }
 }
+readStorageItemEmail();
+// readStorageItemEmail();
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const formElements = event.currentTarget.elements;
+
+//   const email = formElements.email.value;
+//   console.log(email);
+//   const message = formElements.message.value;
+//   console.log(message);
+//   localStorage.setItem('feedback-form-state', email);
+//   if (email.length === 0 || message.length === 0) {
+//     alert('Please fill in all fields');
+//   } else {
+//     const formData = {
+//       email,
+//       message,
+//     };
+//     console.log(formData);
+//     form.reset();
+//   }
+// }
+
+// ======
+
+// const form = document.querySelector(`.feedback-form`);
+// const emailInput = form.querySelector(`input[name="email"]`);
+// const messageInput = form.querySelector(`textarea[name="message"]`);
+// const saveStateToLocalStorage = () => {
+//   const state = {
+//     email: emailInput.value,
+//     message: messageInput.value,
+//   };
+//   localStorage.setItem(`feedback-form-state`, JSON.stringify(state));
+// };
+// const loadStateFromLocalStorage = () => {
+//   const savedState = localStorage.getItem('feedback-form-state');
+//   if (savedState) {
+//     const state = JSON.parse(savedState);
+//     emailInput.value = state.email;
+//     messageInput.value = state.message;
+//   }
+// };
+// const clearFormAndLocalStorage = () => {
+//   const state = {
+//     email: ' ',
+//     message: ' ',
+//   };
+//   localStorage.removeItem('feedback-form-state');
+//   emailInput.value = state.email;
+//   messageInput.value = state.message;
+//   console.log('Form submitted with values:', {
+//     email: emailInput.value,
+//     message: messageInput.value,
+//   });
+// };
+// form.addEventListener('input', throttle(saveStateToLocalStorage, 500));
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   if (emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+//     alert('Будь ласка, заповніть усі поля.');
+//     return;
+//   }
+//   clearFormAndLocalStorage();
+// });
+// loadStateFromLocalStorage();
