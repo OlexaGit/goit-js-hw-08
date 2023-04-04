2fd506e489456469ceb0bb1ac91d7b2e5472a0a9
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-04 11:32:57 +0300

    up-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,31 @@
 const form = document.querySelector('.feedback-form');
+console.log('hello');
+// form.addEventListener('input', event => {
+//   event.preventDefault();
+//   const {
+//     elements: { username, password },
+//   } = event.currentTarget;
+//   console.log(username.value, password.value);
+// });
 
-form.addEventListener('input', event => {
-  event.preventDefault();
-  const {
-    elements: { username, password },
-  } = event.currentTarget;
-  console.log(username.value, password.value);
-});
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  //   event.preventDefault();
+  console.log('hello');
+  const formElements = event.currentTarget.elements;
+  console.log(formElements);
+  const email = formElements.email.value;
+  console.log(email);
+  const message = formElements.message.value;
+  if (email.length === 0 || message.length === 0) {
+    alert('Please fill in all fields');
+  } else {
+    const formData = {
+      email,
+      message,
+    };
+    console.log(formData);
+    // form.reset();
+  }
+}
